87774ea55fb8c760ae42bce226fdcccfd2ec18a5
jackimarques <jacki.marques@hotmail.com>
2024-04-23 21:16:06 -0300

    Alt

---
+++ Fila.java
@@ -0,0 +1,21 @@
+public class Fila {
+
+    private static Fila instancia;
+
+    private Fila() {
+            }
+    public static Fila getInstancia (){
+        if (instancia == null) {
+            instancia = new instancia;
+
+        }
+        return instancia;
+
+    }
+    public void ImprimeDocumento() {
+    }
+    public void RemoveDocumento() {
+    }
+    public void RemoveTodosDocumento() {
+    }
+}
